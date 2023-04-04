7ab0ae9d89ebb68acafa5325d0b209a70dc20f8f
nguyenthanhtrung001 <thanhtrung8674@gmail.com>
2023-04-04 22:37:07 +0700

    update homecontroller

--- src/main/java/management/controller/admin/HomeController.java
+++ src/main/java/management/controller/admin/HomeController.java
@@ -16,6 +16,7 @@ public class HomeController {
 		System.out.println("test1");
 		System.out.println("test2");
 		System.out.println("test3");
+		System.out.println("test4");
 		ModelAndView modelAndView = new ModelAndView("admin/Home");
 		return modelAndView;
 	}
