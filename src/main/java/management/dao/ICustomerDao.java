0d5738d2ccdfad37603ab9c833e24f145e480ff2
QuocTrieu <buiquoctrieu020402@gmail.com>
2023-04-07 02:50:54 +0700

    handle conflict

--- src/main/java/management/dao/ICustomerDao.java
+++ src/main/java/management/dao/ICustomerDao.java
@@ -6,9 +6,9 @@ import management.entity.Customer;
 
 public interface ICustomerDao {
 	Customer createCustomer(Customer customer);
-<<<<<<< HEAD
+
 	Long getNumberOfCustomer();
-=======
-	List<Customer>getListCustomer();
->>>>>>> branch 'master' of https://github.com/quoctrieu020402/Web.git
+
+	List<Customer> getListCustomer();
+
 }
